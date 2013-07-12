6f7bb03a9792895a8c15e91421f450d1444be472
Juan Romero <jgromero@gmail.com>
2013-07-12 01:49:06 +0000

    Modified web for using index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  fs.readFileSync('index.html', function(err, data){
+    if (err) throw err;
+    buf = new Buffer(data);
+    response.send(buf.toString());
+  });
 });
 
 var port = process.env.PORT || 5000;
